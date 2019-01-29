f5d8d7761bafe713f18d2fd475236fae9b6e4e2b
foo-dogsquared <christiangabrielarazas@gmail.com>
2019-01-29 22:42:22 +0800

    Update app skeleton

---
+++ src/app_constants.js
@@ -0,0 +1,18 @@
+exports.app_configuration = {
+    author: "Gabriel Arazas",
+    author_alias: "foo-dogsquared",
+    author_link: "https://foo-dogsquared.github.io/",
+    author_social_links: {
+        github: "https://github.com/foo-dogsquared/",
+        twitter: "https://twitter.com/foo_dogsquared"
+    },
+    app_name: "300 Words A Day",
+    app_description: "An offline app that encourages you to take note every day, 300 words per day.",
+    app_repo_link: "https://github.com/foo-dogsquared/300-words-a-day",
+
+    app_routes: {
+        home: "/",
+        app: "/app",
+        settings: "/settings"
+    }
+}
