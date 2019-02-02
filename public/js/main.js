6a8855635a5aa0bae2acf00d2a4be0ca5c476bf9
foo-dogsquared <christiangabrielarazas@gmail.com>
2019-02-02 22:55:16 +0800

    Make 'public/' as the build directory

--- public/js/main.js
+++
@@ -1,3 +0,0 @@
-function render_date(){}
-if("serviceWorker"in navigator){navigator.serviceWorker.register("/service-worker.js",{scope:"./"}).then(function(registration){console.log(`Service worker has been installed!`)}).catch(function(error){console.log(`Service worker registration failed. See error message:\nERROR: ${error}`)})}
-
