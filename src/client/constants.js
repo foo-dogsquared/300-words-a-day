fef131378a9c0f470b9d44ae5d3fdd30e6f6372a
foo-dogsquared <christiangabrielarazas@gmail.com>
2019-02-05 12:18:56 +0800

    Update the client-side app constants

--- src/client/constants.js
+++ src/client/constants.js
@@ -1,11 +1,19 @@
 const APP_NAME = `_300wads`
 
+// database instance names
+const APP_DB_NAME = `${APP_NAME}_db`
+const APP_SETTINGS_INSTANCE_NAME = `${APP_NAME}_settings`;
+const APP_NOTES_LOCATION_STRING = `${APP_NAME}_notes`;
+
 // autosave settings
 const APP_AUTOSAVE_SETTING = `${APP_NAME}_autosync_interval`;
 const APP_AUTOSAVE_INTERVAL_DEFAULT_VALUE = 60000; // 60 seconds
 
 // markdown flavor settings
 const APP_MARKDOWN_FLAVOR_SETTING = `${APP_NAME}_markdown_flavor`;
+
+// unified notes object name (not editable since it will be stored on IndexedDB)
+const APP_NOTES_INDEX = 0;
 
 // general error messages
 const GET_SELECTION_NOT_SUPPORTED = `Document selection is not supported (for some reason).`;
