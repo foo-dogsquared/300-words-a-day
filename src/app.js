453611aa457dd2eff2ccee886fb6b6e0b75973d4
foo-dogsquared <christiangabrielarazas@gmail.com>
2019-02-06 18:59:30 +0800

    Update source code for Heroku deployment

--- src/app.js
+++ src/app.js
@@ -3,15 +3,19 @@ const pug = require("pug");
 const app_constants = require("./app_constants");
 const app_routes = require("./routes");
 const path = require("path");
-const logger = require("morgan");
+// if the production env is on heroku
+const is_prod_env_heroku = process.env.PRODUCTION_ENV;
+
+// apps deployed on Heroku have their source files stored in https://<HEROKU_APP_LINK>/app/
+// which is why they're somewhat necessary
+const public_dir = (is_prod_env_heroku) ? "../../public" : "../public";
 
 const app = express();
 
 app
     .set("view engine", "pug")
     .set("views", path.join(__dirname, "views"))
-    .use(express.static(path.join(__dirname, "../public")))
-    .use(logger("dev"))
+    .use(express.static(path.join(__dirname, public_dir)))
     .use(app_constants.app_configuration.app_routes.home, app_routes)
     .use("*", function(req, res, next){
         res.status(404).render("layout/error", {
